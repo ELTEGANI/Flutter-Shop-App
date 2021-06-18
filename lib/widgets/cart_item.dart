8017bd3f59da6f1edc5f27ef47186fb70e98002c
altegani mohammed <tiganimoh@gmail.com>
2021-06-18 23:41:14 +0200

    delete cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,33 +1,60 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/product.dart';
 
 
 class CartItem extends StatelessWidget{
 
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id,this.price,this.quantity,this.title);
+  CartItem(this.id,this.productId,this.price,this.quantity,this.title);
 
   @override
   Widget build(BuildContext buildContext) {
-   return Card(margin: EdgeInsets.symmetric(
-     horizontal:15,vertical: 4
-    ),
-   child:Padding(padding: EdgeInsets.all(8),
-     child: ListTile(
-     leading: CircleAvatar(
-       child:Padding(
-           padding:EdgeInsets.all(5),
-           child: FittedBox(child:Text('\$$price'),
-           ),
+   return Dismissible(
+     key: ValueKey(id),
+     background: Container(color:Theme.of(buildContext).errorColor,
+     child: Icon(
+       Icons.delete,
+       color: Colors.white,
+       size: 40,
+     ),
+       alignment: Alignment.centerRight,
+       padding: EdgeInsets.only(right: 20),
+       margin: EdgeInsets.symmetric(
+           horizontal:15,
+           vertical: 4
        ),
      ),
-       title: Text(title),
-       subtitle: Text('Total:\$${(price * quantity)}'),
-       trailing: Text('$quantity x'),
-   ),),
+     direction: DismissDirection.endToStart,
+     onDismissed:(direction){
+        Provider.of<Cart>(buildContext,listen: false).removeItem(productId);
+     },
+     child: Card(
+       margin: EdgeInsets.symmetric(
+       horizontal:15,
+       vertical: 4
+       ),
+     child:Padding(padding: EdgeInsets.all(8),
+       child: ListTile(
+       leading: CircleAvatar(
+         child:Padding(
+             padding:EdgeInsets.all(5),
+             child: FittedBox(child:Text('\$$price'),
+             ),
+         ),
+       ),
+         title: Text(title),
+         subtitle: Text('Total:\$${(price * quantity)}'),
+         trailing: Text('$quantity x'),
+     ),),
+     ),
    );
   }
 
