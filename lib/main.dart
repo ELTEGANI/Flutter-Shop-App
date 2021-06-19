71e739aaa3f574a2642febfcad8f4046a06e2999
altegani mohammed <tiganimoh@gmail.com>
2021-06-19 18:38:25 +0200

    add products

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/orders_screens.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:shop_app/screens/user_product_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products_provider.dart';
 
@@ -37,7 +38,8 @@ class MyApp extends StatelessWidget {
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName:(ctx) => CartScreen(),
-            OrdersScreen.routeName:(ctx) => OrdersScreen()
+            OrdersScreen.routeName:(ctx) => OrdersScreen(),
+            UserProductsScreen.routeName:(ctx) => UserProductsScreen()
           },
         )
     );
